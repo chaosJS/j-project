789ba984e15c186f59d4f65a659745aefa1a2185
chaos_js <lichao779@sina.com>
2018-07-18 21:25:59 +0800

    update script get data

--- js-server/server.js
+++ js-server/server.js
@@ -3,60 +3,75 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
-  console.log('请指定端口号\n node server.js 8888 这样不会吗？')
-  process.exit(1)
+if (!port) {
+	console.log('请指定端口号\n node server.js 8888 这样不会吗？')
+	process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
-  var method = request.method
+var server = http.createServer(function (request, response) {
+	var parsedUrl = url.parse(request.url, true)
+	var path = request.url
+	var query = ''
+	if (path.indexOf('?') >= 0) { query = path.substring(path.indexOf('?')) }
+	var pathNoQuery = parsedUrl.pathname
+	var queryObject = parsedUrl.query
+	var method = request.method
+	console.log('HTTP 路径\n' + path)
+	console.log('查询字符串为\n' + query)
+	console.log('不含查询字符串的路径为\n' + pathNoQuery);
 
-  
-  console.log('HTTP 路径\n' + path)
-  console.log('查询字符串为\n' + query)
-  console.log('不含查询字符串的路径为\n' + pathNoQuery);
-  if(path==='/'){
-	  response.setHeader('Content-Type','text/html;charset=utf-8');
-	  response.write(
-	  `<!DOCTYPE>\n
-	  <html>
-	  <head><title>node page</title>
-	  <link  rel="stylesheet" href="/style"/></head>
-	  <body><h1>你好 node.js</h1><script src="/main"></script>
-	  </body>
-	  </html>
-	  `
-	  );
-	  response.end();
-	  
-  }else if(path==='/style'){
-	   response.setHeader('Content-Type','text/css;charset=utf-8');
-	  response.write(
-	  'body{margin:0;padding:0;} h1{color:#369}'
-	  );
-	  response.end();
-  }else if(path==='/main'){
-	  response.setHeader('Content-Type','text/javascript;charset=utf-8');
-	  response.write(
-	  'alert("this is a js 文件！")'
-	  );
-	  response.end();
-  }  else{
-	  response.statusCode = 404;
-	  response.setHeader('Content-Type','text/text;charset=utf-8');
-	  response.write(
-	  '404 错误'
-	  );
-	  response.end();
-  }
-  
-  
+
+	if (path === '/') {
+		let str = fs.readFileSync('./index.html', 'utf-8');
+		str = str.replace('___amount___', fs.readFileSync('./db', 'utf-8'));
+		response.setHeader('Content-Type', 'text/html;charset=utf-8');
+		response.write(str);
+		response.end();
+
+	} else if (path === '/style') {
+		let str = fs.readFileSync('./style.css', 'utf-8');
+
+		response.setHeader('Content-Type', 'text/css;charset=utf-8');
+		response.write(
+			str
+		);
+		response.end();
+	} else if (path === '/main') {
+		let str = fs.readFileSync('./main.js', 'utf-8');
+		response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
+		response.write(
+			str
+		);
+		response.end();
+	} else if (path === '/pay') {
+		let amount = fs.readFileSync('./db', 'utf-8');
+		var newAmount = amount - 1;
+		if (Math.random() > 0.2) {
+			fs.writeFileSync('./db', newAmount);
+			response.setHeader('Content-Type', 'application/javascript');
+			response.statusCode = 200;
+			response.write(
+				`
+			amount.innerText =${newAmount}
+			`
+			);
+		} else {
+			response.statusCode = 400;
+
+			response.write('fail');
+		}
+		response.end();
+
+	} else {
+		response.statusCode = 404;
+		response.setHeader('Content-Type', 'text/text;charset=utf-8');
+		response.write(
+			'404 错误'
+		);
+		response.end();
+	}
+
+
 })
 
 server.listen(port)
