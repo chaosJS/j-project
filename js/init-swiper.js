f04d232005483bfd38b19ef34ad00a38c6e7a9ad
chaos_js <lichao779@sina.com>
2018-07-23 21:04:50 +0800

    update mvc swiper

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,13 +1,32 @@
 !function () {
-    var swiper = new Swiper('.swiper-container', {
-        effect: 'flip',
-        grabCursor: true,
-        pagination: {
-            el: '.swiper-pagination',
+
+    var view = document.querySelector('#mySlides');
+    var controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
         },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        init: function (view) {
+            this.view = view;
+            this.initSwiper();
         },
-    });
+        initSwiper: function () {
+            this.swiper = new Swiper(
+                this.view.querySelector('.swiper-container'),
+                this.swiperOptions
+            );
+        }
+    }
+
+    controller.init.call(controller, view);
+
 }.call()
