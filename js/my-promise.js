558de8f120304edc46fba4ae5cab841d1cc3112f
chaos_js <lichao779@sina.com>
2018-07-27 22:43:18 +0800

    update promise

---
+++ js/my-promise.js
@@ -0,0 +1,27 @@
+function getNumber() {
+    return new Promise(function (resolve, reject) {
+        //做一些异步操作
+        setTimeout(function () {
+            var num = Math.ceil(Math.random() * 10); //生成1-10的随机数
+            if (num <= 5) {
+                resolve(num);
+            }
+            else {
+                reject('数字太大了');
+            }
+        }, 1000);
+    });
+
+}
+
+getNumber()
+    .then(
+        function (data) {
+            console.log('resolved');
+            console.log(data);
+        },
+        function (reason) {
+            console.log('rejected');
+            console.log(reason);
+        }
+    );
